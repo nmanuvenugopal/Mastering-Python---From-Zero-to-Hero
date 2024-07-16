c7551ed339274e5e6db7b844a8609639fe5f35fc
Manu Venugopalan <99719105+nmanuvenugopal@users.noreply.github.com>
2024-07-16 18:50:34 +0100

    Update banker_roulette.py

--- Randomization and Python Lists/banker_roulette.py
+++ Randomization and Python Lists/banker_roulette.py
@@ -1,1 +1,12 @@
+import random
 
+
+names = "Angela, Ben, Jenny, Michael, Chloe"
+
+names_list = names.split(",")
+print(names_list)
+
+random_num = random.randint(0, len(names_list))
+pay_person  = names_list.pop(random_num)
+
+print(f"{pay_person} is going to buy the meal today")
