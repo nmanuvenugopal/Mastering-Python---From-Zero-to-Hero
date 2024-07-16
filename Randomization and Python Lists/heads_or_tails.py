c9eed0255a3d0ed5cf771191527aff6f5da3c4c8
Manu Venugopalan <99719105+nmanuvenugopal@users.noreply.github.com>
2024-07-16 16:32:46 +0100

    Update heads_or_tails.py

--- Randomization and Python Lists/heads_or_tails.py
+++ Randomization and Python Lists/heads_or_tails.py
@@ -1,1 +1,9 @@
+import random
+# random.randint() will generate a random integer
+# random.random() will generate a random floating point number.
 
+random_num  = random.randint(0,1)
+if random_num == 1:
+    print("Heads")
+else:
+    print("Tails")
